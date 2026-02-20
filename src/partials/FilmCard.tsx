8779b1c257184097d4999f85a8f3d96bf3adc998
FatimaAlmurtadha <sukaina77150@gmail.com>
2026-02-20 09:56:12 +0100

    making the film poster clickable instead of a button

--- src/partials/FilmCard.tsx
+++ src/partials/FilmCard.tsx
@@ -5,14 +5,15 @@ import type Film from "../interfaces/Film";
 export default function FilmCard({ film }: { film: Film }) {
   return (
     <div className="film-card shadow-sm rounded p-2">
-      <Link to={`/filmer/${film.id}`}>
+      <Link className="film-card shadow-sm rounded p-2 text-decoration-none text-dark" style={{
+        display: "block",
+        cursor: "pointer"
+      }} to={`/filmer/${film.id}`}
+      >
         <img className="w-100 rounded" src={film.poster_url} alt={film.title} />
       </Link>
       <h5 className="mt-2">{film.title}</h5> 
       <p className="text-muted">{film.genre}</p>
-      <Link className="btn btn-primary w-100 mt-2" to={`/films/${film.id}`}>
-        Visa detaljer
-      </Link>
     </div>
   );
 } 
