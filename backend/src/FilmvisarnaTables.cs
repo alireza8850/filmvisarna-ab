a2ea89980d483bd8d0dc67022dc76e46c65b3a52
FatimaAlmurtadha <sukaina77150@gmail.com>
2026-02-24 00:28:42 +0100

    Done with images and trailers' url

--- backend/src/FilmvisarnaTables.cs
+++ backend/src/FilmvisarnaTables.cs
@@ -198,31 +198,36 @@ public static class FilmvisarnaTables
       var filmsData = new List<string>
             {
                 @"INSERT INTO films (title, duration_minutes, genre, release_year, age_limit, description, language, poster_url, trailer_url) VALUES
-                ('Avatar 3', 192, 'Science fiction', 2025, 12,'Den tredje delen i Avatar-sagan där konflikten på Pandora når nya nivåer och nya allianser formas.','Svenska', 'avatar3.jpg', 'avatar3_trailer.mp4')",
+                ('Avatar 3', 192, 'Science fiction', 2025, 12,'Den tredje delen i Avatar-sagan där konflikten på Pandora når nya nivåer och nya allianser formas.','Svenska', 'avatar3.jpg', 'https://www.youtube.com/watch?v=nb_fFj_0rq8&pp=ygUHYXZhdGFyMw%3D%3D')",
                 @"INSERT INTO films (title, duration_minutes, genre, release_year, age_limit, description, language, poster_url, trailer_url) VALUES
-                ('Mercy', 105, 'Thriller', 2023, 15,'En före detta soldat tvingas rädda sin familj i en desperat kamp mot klockan.','Svenska', 'mercy.jpg', 'mercy_trailer.mp4')",
+                ('Mercy', 105, 'Thriller', 2023, 15,'En före detta soldat tvingas rädda sin familj i en desperat kamp mot klockan.','Svenska', 'mercy.jpg', 'https://www.youtube.com/watch?v=7H7Djx17l8Y&pp=ygUFbWVyY3k%3D')",
                 @"INSERT INTO films (title, duration_minutes, genre, release_year, age_limit, description, language, poster_url, trailer_url) VALUES
-                ('SvampBob Fyrkant – Filmen', 95, 'Animerat', 2023, 7,'SvampBob ger sig ut på ett nytt galet undervattensäventyr tillsammans med sina vänner.','Svenska', 'spongebob.jpg', 'spongebob_trailer.mp4')",
+                ('SvampBob Fyrkant', 95, 'Animerat', 2023, 7,'SvampBob ger sig ut på ett nytt galet undervattensäventyr tillsammans med sina vänner.','Svenska', 'spongebob.jpg', 'https://www.youtube.com/watch?v=XdPt8QWTypI&pp=ygUTc3BvbmdlYm9iMjAyMyBtb3ZpZQ%3D%3D')",
                 @"INSERT INTO films (title, duration_minutes, genre, release_year, age_limit, description, language, poster_url, trailer_url) VALUES
-                ('Greenland 2', 120, 'Action', 2024, 11,'Efter en global katastrof ställs mänskligheten inför ett nytt hot mot sin överlevnad.','Svenska', 'greenland2.jpg', 'greenland2_trailer.mp4')",
+                ('Greenland 2', 120, 'Action', 2024, 11,'Efter en global katastrof ställs mänskligheten inför ett nytt hot mot sin överlevnad.','Svenska', 'greenland2.jpg', 'https://www.youtube.com/watch?v=hiD3zk0ZRFg&pp=ygUQZ3JlZW5sYW5nMiBtb3ZpZQ%3D%3D')",
                 @"INSERT INTO films (title, duration_minutes, genre, release_year, age_limit, description, language, poster_url, trailer_url)  VALUES
-                ('Send Help', 100, 'Skräck', 2024, 16,'Två överlevande kämpar för att hålla sig vid liv efter en brutal flygolycka.','Svenska', 'sendhelp.jpg', 'sendhelp_trailer.mp4')",
+                ('Send Help', 100, 'Skräck', 2024, 16,'Två överlevande kämpar för att hålla sig vid liv efter en brutal flygolycka.','Svenska', 'sendhelp.jpg', 'https://www.youtube.com/watch?v=R4wiXj9NmEE&pp=ygUPc2VuZCBoZWxwIG1vdmll')",
                 @"INSERT INTO films (title, duration_minutes, genre, release_year, age_limit, description, language, poster_url, trailer_url) VALUES
-                ('28 Years Later', 130, 'Skräck', 2025, 18,'Årtionden efter virusutbrottet återvänder skräcken i en ännu brutalare värld.','Svenska', '28yearslater.jpg', '28yearslater_trailer.mp4')",
+                ('28 Years Later', 130, 'Skräck', 2025, 18,'Årtionden efter virusutbrottet återvänder skräcken i en ännu brutalare värld.','Svenska', '28yearslater.jpg', 'https://www.youtube.com/watch?v=mcvLKldPM08&pp=ygUSMjh5ZWFyc2xhdGVyIG1vdmll')",
                 @"INSERT INTO films (title, duration_minutes, genre, release_year, age_limit, description, language, poster_url, trailer_url) VALUES
-                ('Die My Love', 110, 'Drama', 2024, 15,'Ett intensivt psykologiskt drama om kärlek, besatthet och sammanbrott.','Svenska', 'diemylove.jpg', 'diemylove_trailer.mp4')",
+                ('Die My Love', 110, 'Drama', 2024, 15,'Ett intensivt psykologiskt drama om kärlek, besatthet och sammanbrott.','Svenska', 'diemylove.jpg', 'https://www.youtube.com/watch?v=ol822Dp0ngQ&pp=ygURZGllIG15IGxvdmUgbW92aWU%3D')",
                 @"INSERT INTO films (title, duration_minutes, genre, release_year, age_limit, description, language, poster_url, trailer_url) VALUES
-                ('Eagles of the Republic', 125, 'Drama', 2023, 13,'En politisk thriller om makt, lojalitet och identitet i en skakig nation.','Svenska', 'eagles.jpg', 'eagles_trailer.mp4')",
+                ('Eagles of the Republic', 125, 'Drama', 2023, 13,'En politisk thriller om makt, lojalitet och identitet i en skakig nation.','Svenska', 'eagles.jpg', 'https://www.youtube.com/watch?v=U0KKCGsqs8I&pp=ygUMZWFnbGVzIG1vdmll')",
                 @"INSERT INTO films (title, duration_minutes, genre, release_year, age_limit, description, language, poster_url, trailer_url) VALUES
-                ('No Other Choice', 118, 'Kriminal', 2023, 15,'En man pressas till det yttersta när han tvingas fatta omöjliga beslut.','Svenska', 'nootherchoice.jpg', 'nootherchoice_trailer.mp4')",
+                ('No Other Choice', 118, 'Kriminal', 2023, 15,'En man pressas till det yttersta när han tvingas fatta omöjliga beslut.','Svenska', 'nootherchoice.jpg', 'https://www.youtube.com/watch?v=HKZpuG_ezvY&pp=ygUVbm8gb3RoZXIgY2hvaWNlIG1vdmll')",
                 @"INSERT INTO films (title, duration_minutes, genre, release_year, age_limit, description, language, poster_url, trailer_url) VALUES
-                ('Nuremberg', 135, 'Historiskt', 2023, 13,'En dramatisk skildring av rättegångarna mot nazistiska krigsförbrytare efter andra världskriget.','Svenska', 'nuremberg.jpg', 'nuremberg_trailer.mp4')",
+                ('Nuremberg', 135, 'Historiskt', 2023, 13,'En dramatisk skildring av rättegångarna mot nazistiska krigsförbrytare efter andra världskriget.','Svenska', 'nuremberg.jpg', 'https://www.youtube.com/watch?v=WvAy9C-bipY&pp=ygUPbnVyZW1iZXJnIG1vdmll')",
                 @"INSERT INTO films (title, duration_minutes, genre, release_year, age_limit, description, language, poster_url, trailer_url) VALUES
-                ('Dune: Del två', 165, 'Science fiction', 2024, 12,'Paul Atreides förenar Fremenfolket och tar kampen om planeten Arrakis.','Svenska', 'dune2.jpg', 'dune2_trailer.mp4')",
+                ('Dune: Del två', 165, 'Science fiction', 2024, 12,'Paul Atreides förenar Fremenfolket och tar kampen om planeten Arrakis.','Svenska', 'dune2.jpg', 'https://www.youtube.com/watch?v=BEO2D_hhXDs&pp=ygULZHVuZTIgbW92aWXSBwkJogoBhyohjO8%3D')",
                 @"INSERT INTO films (title, duration_minutes, genre, release_year, age_limit, description, language, poster_url, trailer_url) VALUES
-                ('Oppenheimer', 180, 'Biografi', 2023, 15,'Historien om J. Robert Oppenheimer och skapandet av atombomben.','Svenska', 'oppenheimer.jpg', 'oppenheimer_trailer.mp4')",
+                ('Oppenheimer', 180, 'Biografi', 2023, 15,'Historien om J. Robert Oppenheimer och skapandet av atombomben.','Svenska', 'oppenheimer.jpg', 'https://www.youtube.com/watch?v=bK6ldnjE3Y0&pp=ygURb3BwZW5oZWltZXIgbW92aWU%3D')",
                 @"INSERT INTO films (title, duration_minutes, genre, release_year, age_limit, description, language, poster_url, trailer_url) VALUES
-                ('The Batman', 176, 'Action', 2022, 13,'Batman utreder en serie mörka brott som hotar hela Gotham City.','Svenska', 'batman.jpg', 'batman_trailer.mp4')"
+                ('The Batman', 176, 'Action', 2022, 13,'Batman utreder en serie mörka brott som hotar hela Gotham City.','Svenska', 'batman.jpg', 'https://www.youtube.com/watch?v=mqqft2x_Aa4&pp=ygUQdGhlIGJhdG1hbiBtb3ZpZQ%3D%3D')",
+                @"INSERT INTO films (title, duration_minutes, genre, release_year, age_limit, description, language, poster_url, trailer_url) VALUES
+                ('Bamse och havets hemlighet', 66, 'Barn, Animerat, Familj', 2025, 19,'Bamse och havets hemlighet är en film som handlar om att vara utanför, inte ha en kompis och om den underbara lyckan i att äntligen hitta någon som förstår en. Det är en storslagen historia som tar Bamse, Lille Skutt och Skalman bort från Småköping, ut på öppet vatten, till andra sidan jordklotet och långt ner på havets botten. Sist men inte minst är det en berättelse om kapten Buster som är beredd att göra vad som helst för att få tillbaka titeln Havets fasa. Precis vad som helst. Ingenting kan stoppa honom från att försöka vara tuffast på havet. Inte ens världens största sjöodjur.','Svenska', 'bamse.jpg', 'https://www.youtube.com/watch?v=exPw1q2n6q8&pp=ygUgQmFtc2Ugb2NoIGhhdmV0cyBoZW1saWdoZXQgbW92aWXSBwkJogoBhyohjO8%3D')",
+                @"INSERT INTO films (title, duration_minutes, genre, release_year, age_limit, description, language, poster_url, trailer_url) VALUES
+                ('Lek med Alfons Åberg', 37, 'Barn, Animerat, Familj', 2026, 16,'Ett nytt lekfullt Bok-Filmspaket med Alfons Åberg. Det handlar om det alldeles underbara när pappa äntligen lägger vardagssysslorna åt sidan och följer med Alfons in i fantasins värld, om att leka osynligt med Viktor men också om det hemska i att bli oskyldigt anklagad för att vara tjuv. Vi får möta Alfons i Osynligt med Alfons, Där går TJUV-Alfons! och Flyg! Sa Alfons Åberg','Svenska', 'lek.jpg', 'https://www.youtube.com/watch?v=wWB5XP-abuI&pp=ygUbTGVrIG1lZCBBbGZvbnMgw4ViZXJnIG1vdmll')"
+
             };
       foreach (var sql in filmsData)
       {
