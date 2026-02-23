92b790e1fbac81528144fb9c31a05075f070ef01
FatimaAlmurtadha <sukaina77150@gmail.com>
2026-02-23 22:56:06 +0100

    Put the right path for the landed page images

--- src/partials/FilmCard.tsx
+++ src/partials/FilmCard.tsx
@@ -5,11 +5,14 @@ import type Film from "../interfaces/Film";
 export default function FilmCard({ film }: { film: Film }) {
   return (
     <div className="film-card">
-      <Link className="film-card__link" to={`/films/${film.id}`}
-      >
-        <img className="film-card__image" src={film.poster_url} alt={film.title} />
+      <Link className="film-card__link" to={`/films/${film.id}`}>
+        <img
+          className="film-card__image"
+          src={"/images/" + film.poster_url}
+          alt={film.title}
+        />
       </Link>
-      <h5 className="film-card__title">{film.title}</h5> 
+      <h5 className="film-card__title">{film.title}</h5>
       <p className="film-card__genre">{film.genre}</p>
     </div>
   );
