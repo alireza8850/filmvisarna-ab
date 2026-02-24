3973e91c7d13a47e08a8df976f1ffd8d976e54d3
arbazshah52 <arbazexample.com>
2026-02-24 15:41:06 +0100

    select seat

--- src/routes.ts
+++ src/routes.ts
@@ -9,6 +9,8 @@ import ProductDetailsPage from "./pages/ProductDetailsPage.tsx";
 import ProductsPage from "./pages/ProductsPage.tsx";
 import FilmDetailsPage from "./pages/FilmDetailsPage.tsx";
 import LandedPageFilms from "./pages/LandedPageFilms.tsx";
+import SeatSelectionPage from "./pages/SeatSelectionPage.tsx";
+
 
 interface Route {
   element: JSX.Element;
@@ -28,6 +30,7 @@ export default [
   ProductsPage,
   LandedPageFilms,
   FilmDetailsPage,
+  SeatSelectionPage,
 ]
   // map the route property of each page component to a Route
   .map((x) => ({ element: createElement(x), ...x.route }) as Route)
