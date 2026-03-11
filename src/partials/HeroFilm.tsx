d05f6489b100ad7eaef49d16dd03344c18f6d3a9
OskarUNLEASHED <oskar.gyllenor@gmail.com>
2026-03-11 23:26:08 +0100

    Forgot to fix this :^)

--- src/partials/HeroFilm.tsx
+++ src/partials/HeroFilm.tsx
@@ -8,37 +8,37 @@ export default function HeroFilm({ film }: { film: Film }) {
 
   return (
     <div className="hero-film-wrapper">
-      <section className="hero-film">
-        {/* --- The hero film will be shown as a big banner with the poster image as background --- */}
+      <Link to={`/films/${film.id}`} className="hero-film-link">
+        <section className="hero-film">
+          {/* --- The hero film will be shown as a big banner with the poster image as background --- */}
 
-        <figure className="hero-media">
-          <Link to={`/films/${film.id}`}>
+          <figure className="hero-media">
             <img
               className="hero-image"
               src={imageUrl}
               alt={film.title}
             />
-          </Link>
-        </figure>
-        <header className="hero-overlay">
-          <div className="hero-top">
-            <Link to={`/films/${film.id}`} className="hero-title-link">
-              <h1 className="hero-title">{film.title}</h1>
-            </Link>
-            <br />
-            <span className="btn-sections">
-              {/* --- The button will be shown as a red button with white text --- */}
-              <Link className="hero-trailer-btn" to={`/films/${film.id}`}>
-                Trailer
-              </Link>
-              {/* --- We will show the genre as a badge, the title as a heading and a button to watch the trailer --- */}
-              <span className="hero-genre">{film.genre}</span>
-            </span>
-          </div>
+          </figure>
+          <header className="hero-overlay">
+            <div className="hero-top">
+              <div className="hero-title-container">
+                <h1 className="hero-title">{film.title}</h1>
+              </div>
+              <br />
+              <span className="btn-sections">
+                {/* --- The button will be shown as a red button with white text --- */}
+                <span className="hero-trailer-btn">
+                  Trailer
+                </span>
+                {/* --- We will show the genre as a badge, the title as a heading and a button to watch the trailer --- */}
+                <span className="hero-genre">{film.genre}</span>
+              </span>
+            </div>
 
-          {/* --- The title will be shown as a big heading --- */}
-        </header>
-      </section>
+            {/* --- The title will be shown as a big heading --- */}
+          </header>
+        </section>
+      </Link>
     </div>
   );
 } 
