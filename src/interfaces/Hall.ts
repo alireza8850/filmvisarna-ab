bf14def3338000d212e5d8d58e26ab362d6e2ad0
Nehaasati <31asatineha@gmail.com>
2026-03-11 14:25:29 +0100

    add hall interface with new table

--- src/interfaces/Hall.ts
+++ src/interfaces/Hall.ts
@@ -1,6 +1,15 @@
 export default interface Hall {
   id: number;
   hall_name: string;
+  hall_description: string;
+  halls_image: string;
+  audio_name: string;
+  audio_description: string;
+  audio_image: string;
+  food_name: string;
+  food_description: string;
+  glasses_name: string;
+  glasses_description: string;
   total_rows: number;
   seats_per_row: number;
 }
