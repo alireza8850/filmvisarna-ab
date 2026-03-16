67f43d9dad2256c73ee9b779493aa75188b43c53
OskarUNLEASHED <oskar.gyllenor@gmail.com>
2026-03-16 11:04:03 +0100

    Finished admin part and clickable hero but things seem to be tweaking

--- src/partials/HeroFilm.tsx
+++ src/partials/HeroFilm.tsx
@@ -1,42 +1,43 @@
+
 import { Link } from "react-router-dom";
 import type Film from "../interfaces/Film";
 
 export default function HeroFilm({ film }: { film: Film }) {
-  const imageUrl = film.poster_url.startsWith('http') 
-    ? film.poster_url 
-    : "/images/" + film.poster_url;
+    const imageUrl = film.poster_url.startsWith('http')
+        ? film.poster_url
+        : "/images/" + film.poster_url;
 
-  return (
-    <div className="hero-film-wrapper">
-      <section className="hero-film">
-        {/* --- The hero film will be shown as a big banner with the poster image as background --- */}
+    return (
+        <div className="hero-film-wrapper">
+            <Link to={`/films/${film.id}`} style={{ textDecoration: "none", display: "block" }}>
+                <section className="hero-film">
+                    {/* --- The hero film will be shown as a big banner with the poster image as background --- */}
 
-        <figure className="hero-media">
-          <Link to={`/films/${film.id}`}>
-            <img
-              className="hero-image"
-              src={imageUrl}
-              alt={film.title}
-            />
-          </Link>
-        </figure>
-        <header className="hero-overlay">
-          <div className="hero-top">
-            <Link to={`/films/${film.id}`} className="hero-title-link">
-              <h1 className="hero-title">{film.title}</h1>
-            </Link>
-            <br />
-            <span className="btn-sections">
-              <span className="hero-trailer-btn">
-                Trailer
+                    <figure className="hero-media">
+                        <img
+                            className="hero-image"
+                            src={imageUrl}
+                            alt={film.title}
+                        />
+                    </figure>
+                    <header className="hero-overlay">
+                        <div className="hero-top">
+              <span className="hero-title-link">
+                <h1 className="hero-title">{film.title}</h1>
               </span>
-              <span className="hero-genre">{film.genre}</span>
-            </span>
-          </div>
+                            <br />
+                            <span className="btn-sections">
+                <span className="hero-trailer-btn">
+                  Trailer
+                </span>
+                <span className="hero-genre">{film.genre}</span>
+              </span>
+                        </div>
 
-          {/* --- The title will be shown as a big heading --- */}
-        </header>
-      </section>
-    </div>
-  );
-} 
+                        {/* --- The title will be shown as a big heading --- */}
+                    </header>
+                </section>
+            </Link>
+        </div>
+    );
+}
